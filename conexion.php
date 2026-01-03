27f4b553f6d9e0eb438f876076c71abd0047480f
luis1ultimo <luisevelio065@gmail.com>
2026-01-03 12:13:33 -0500

    cambio1

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("localhost", "root", "", "pasteleria");
+$conexion = new mysqli("sql207.infinityfree.com", "if0_40817464", "luisevelio065", "if0_40817464_pasteleria33");
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
